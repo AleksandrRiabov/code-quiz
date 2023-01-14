4b8cbf8c24f1c52665c10886a2f80b528bd25900
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-14 05:41:35 +0000

    More comments added

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,36 +1,38 @@
-const highScoresList = document.querySelector('#highscores');
-const clearBtn = document.querySelector('#clear');
+const highScoresList = document.querySelector("#highscores");
+const clearBtn = document.querySelector("#clear");
 
 renderAllResults();
 
-clearBtn.addEventListener('click', () => {
-  localStorage.removeItem('results');
+//to clear results from local storage on click
+clearBtn.addEventListener("click", () => {
+  localStorage.removeItem("results");
   renderAllResults();
 });
 
-
 function renderAllResults() {
   //Reset ordered list
-  highScoresList.innerHTML = '';
+  highScoresList.innerHTML = "";
 
   //Get results from local storage
-  const results = JSON.parse(localStorage.getItem('results')) || [];
-  //if no results saved in local storage 
+  const results = JSON.parse(localStorage.getItem("results")) || [];
+  //if no results saved in local storage
   if (!results.length) {
-    highScoresList.textContent  = 'No results to display..'
-    return
+    highScoresList.textContent = "No results to display..";
+    return;
   }
 
   //Sort results array from highest score to the lowest
-  const sortedResults = results.sort((a, b) => parseInt(a.score) - parseInt(b.score)).reverse();
+  const sortedResults = results
+    .sort((a, b) => parseInt(a.score) - parseInt(b.score))
+    .reverse();
 
   //Render all results
-  sortedResults.forEach(result => renderListItem(result));
+  sortedResults.forEach((result) => renderListItem(result));
 }
 
 //=== Function to create single list item with result
 function renderListItem(result) {
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.textContent = `${result.initials} - ${result.score}`;
   highScoresList.append(li);
 }
