c0ec8cab09a85198c1d335bec344b878fd08416b
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-14 03:00:58 +0000

    Quiz starting page and quiz questions page completed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,36 +2,109 @@ const timeSpan = document.querySelector('#time');
 const startBtn = document.querySelector('#start');
 const questionsContainer = document.querySelector('#questions');
 const questionTitle = document.querySelector('#question-title');
+const choices = document.querySelector('#choices');
+const feedback = document.querySelector('#feedback');
+const startScreen = document.querySelector('#start-screen');
+const endScreen = document.querySelector('#end-screen');
+const finalScore = document.querySelector('#final-score');
+const initials = document.querySelector('#initials');
+const submitBtn = document.querySelector('#submit');
 
-
-let timeLeft = 5;
-
-
-
-
+let timeLeft = 95;
+let score = 0;
+let timerId;
 
 const startTimer = () => {
-  const timerId = setInterval( () => {
-    if (timeLeft > 0 ) {
+  timerId = setInterval(() => {
+    if (timeLeft > 0) {
       timeLeft--;
       timeSpan.textContent = timeLeft;
     } else {
       clearInterval(timerId);
-      console.log('No more time left!')
-      // window.location.href = './highscores.html'
+      window.location.href = './highscores.html';
     }
-  },1000);
+  }, 1000);
 }
 
+//Start quiz
 const startTheQuiz = () => {
-  questionsContainer.class = '';
-  const askQuestion = (question) => {
+  startScreen.classList.add('hide');
+  startTimer();
+  askQuestion(questions[0]);
+  checkTheAnswer(questions[0], 0)
+}
 
-    questionTitle.textContent = question.question; 
+startBtn.addEventListener('click', startTheQuiz);
+
+
+function checkTheAnswer(question, questionIndex) {
+
+  choices.addEventListener('click', checkResult);
+
+  function checkResult(e) {
+    //Check if user selected answer  = correct answer
+    if (e.target.dataset.index == question.correctAnswer) {
+      score++;
+      localStorage.setItem('score', score);
+      feedback.textContent = 'Correct!'
+    } else {
+      timeLeft -= 15;
+      feedback.textContent = 'Wrong!'
+    }
+    //Display question feedback
+    feedback.classList.remove('hide');
+
+    //Removed event listener
+    choices.removeEventListener('click', checkResult);
+
+    //If more questions available repeat the proccess. Ask question and check question.
+    //Else display results
+    if (questionIndex + 1 === questions.length) {
+      clearInterval(timerId);
+      displayResults();
+    } else {
+      askQuestion(questions[questionIndex + 1]);
+      checkTheAnswer(questions[questionIndex + 1], questionIndex + 1);
+    }
   }
-
-  askQuestion(questions[1])
 }
 
 
-startBtn.addEventListener('click', startTimer);
+//Display question and variants
+function askQuestion(question) {
+  //Unhide if hidden
+  if (questionsContainer.classList.contains('hide')) {
+    questionsContainer.classList.remove('hide');
+  }
+  //Display question
+  questionTitle.textContent = question.question;
+  //Clear previous variants and populate current question variants
+  choices.innerHTML = '';
+  question.answers.forEach((answer, index) => {
+    const variant = document.createElement('button');
+    variant.textContent = `${index + 1}. ${answer}`;
+    variant.dataset.index = index;
+    choices.append(variant);
+  });
+}
+
+function displayResults() {
+  questionsContainer.classList.add('hide');
+  endScreen.classList.remove('hide');
+  finalScore.textContent = score;
+}
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,22 +1,82 @@
 const questions = [
   {
-    question: 'How to use keyboard?',
-    answers: ["I don't know", "Press the key", "Some answer"],
-    corerctAnswer: "Some answer"
+    question: 'What is JSON??',
+    answers: [
+      "JavaScript Object Nutrition",
+      "Java Standard Object Number",
+      "JavaScript Object Notation",
+      "JavaScript Object Notification"
+    ],
+    correctAnswer: 2
   },
   {
-    question: 'How to use keyboard?',
-    answers: ["I don't know", "Press the key", "Some answer"],
-    corerctAnswer: "Some answer"
+    question: 'JavaScript is a ___ -side programming language.?',
+    answers: [
+      "Client",
+      "Server",
+      "Both",
+      "None"
+    ],
+    correctAnswer: 2
   },
   {
-    question: 'How to use keyboard?',
-    answers: ["I don't know", "Press the key", "Some answer"],
-    corerctAnswer: "Some answer"
+    question: 'Which of the following will write the message “Hello DataFlair!” in an alert box?',
+    answers: [
+      "alertBox(“Hello DataFlair!”);",
+      "alert(Hello DataFlair!);",
+      "msgAlert(“Hello DataFlair!”);",
+      "alert(“Hello DataFlair!”);"
+    ],
+    correctAnswer: 3
   },
   {
-    question: 'How to use keyboard?',
-    answers: ["I don't know", "Press the key", "Some answer"],
-    corerctAnswer: "Some answer"
+    question: 'How do you find the minimum of x and y using JavaScript?',
+    answers: [
+      "min(x,y);",
+      "Math.min(x,y)",
+      "Math.min(xy)",
+      "min(xy);"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: 'Which JavaScript label catches all the values, except for the ones specified?',
+    answers: [
+      "catch",
+      "label",
+      "try",
+      "default"
+    ],
+    correctAnswer: 3
+  },
+  {
+    question: 'Which are the correct “if” statements to execute certain code if “x” is equal to 2?',
+    answers: [
+      "if(x 2)",
+      "if(x = 2)",
+      "if(x == 2)",
+      "if(x !== 2)"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: 'What will the code return? Boolean(3 < 7)',
+    answers: [
+      "Null",
+      "true",
+      "NaN",
+      "SyntaxError;"
+    ],
+    correctAnswer: 1
+  },
+  {
+    question: 'Which is the correct JavaScript syntax to change the HTML content given below?',
+    answers: [
+      "document.getElementById(“test”).innerHTML = “Hello DataFlair!”;",
+      "document.getElementsById(“test”).innerHTML = “Hello DataFlair!”;",
+      "document.getElementById(test).innerHTML = “Hello DataFlair!”;",
+      "mdocument.getElementByTagName(“p”)[0].innerHTML = “Hello DataFlair!”;"
+    ],
+    correctAnswer: 0
   }
 ]
